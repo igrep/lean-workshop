4e984221866f4c6d06682e2628a0ad24bed7654b
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2025-06-17 22:04:40 +0900

    2025/06/17

--- Test/IndProp.lean
+++ Test/IndProp.lean
@@ -1,4 +1,6 @@
 import Test.Basic
+import Test.Induction
+import Test.Logic
 
 def div2 (n : Nat) : Nat :=
   match n with
@@ -123,3 +125,200 @@ def cs (n m : Nat) : Prop := csf n = m
 def cms n m := clos_refl_trans cs n m
 
 
+inductive clos_refl_trans_sym
+  {X: Type}
+  (R: X → X → Prop)
+  : X → X → Prop where
+  | rt_step
+    {x y : X}
+    (h : R x y)
+    : clos_refl_trans_sym R x y
+  | rt_refl
+    {x : X}
+    : clos_refl_trans_sym R x x
+  | rt_symm
+    {x y : X}
+    (h : clos_refl_trans_sym R x y)
+    : clos_refl_trans_sym R y x
+  | rt_trans
+    {x y z : X}
+    (h1 : clos_refl_trans_sym R x y)
+    (h2 : clos_refl_trans_sym R y z)
+    : clos_refl_trans_sym R x z
+
+inductive Perm3 {X : Type} : List X → List X → Prop where
+  | swap12 (a b c : X) :
+      Perm3 [a, b, c] [b, a, c]
+  | swap23 (a b c : X) :
+      Perm3 [a, b, c] [a, c, b]
+  | trans (l1 l2 l3 : List X) :
+      Perm3 l1 l2 → Perm3 l2 l3 → Perm3 l1 l3
+
+example : Perm3 [1, 2, 3] [3, 2, 1] := by
+  apply Perm3.trans (l2 := [2, 3, 1])
+  · apply Perm3.trans (l2 := [2, 1, 3])
+    · apply Perm3.swap12
+    · apply Perm3.swap23
+  · apply Perm3.swap12
+  done
+
+example : Perm3 [1, 2, 3] [1, 2, 3] := by
+  apply Perm3.trans (l2 := [1, 3, 2])
+  · apply Perm3.swap23
+  · apply Perm3.swap23
+  done
+
+#check Trans
+
+inductive ev : Nat → Prop where
+  | zero : ev 0
+  | succ {n : Nat} (h : ev n) : ev (n + 2)
+
+#check (ev.zero : ev 0)
+#check (ev.succ ev.zero : ev 2)
+
+namespace EvPlayground
+  inductive ev : Nat → Prop where
+    | ev_0 : ev 0
+    | ev_SS : ∀ (n : Nat), ev n → ev (n + 2)
+end EvPlayground
+
+theorem ev_4 : ev 4 := by
+  apply ev.succ
+  apply ev.succ
+  apply ev.zero
+  done
+
+theorem ev_4' : ev 4 := by
+  apply (ev.succ (ev.succ ev.zero))
+  done
+
+theorem ev_plus4 : ∀ n, ev n → ev (n + 4) := by
+  intro n h
+  apply ev.succ
+  apply ev.succ
+  apply h
+  done
+
+theorem ev_double : ∀ n, ev (double n) := by
+  intro n
+  induction n
+  case zero =>
+    apply ev.zero
+  case succ n ih =>
+    apply ev.succ
+    apply ih
+  done
+
+theorem Perm3_ex1 : Perm3 [1, 2, 3] [2, 3, 1] := by
+  apply Perm3.trans (l2 := [2, 1, 3])
+  · apply Perm3.swap12
+  · apply Perm3.swap23
+  done
+
+theorem Perm3_refl : ∀ (X : Type) (a b c : X),
+  Perm3 [a, b, c] [a, b, c] := by
+  intro X a b c
+  apply Perm3.trans (l2 := [a, c, b])
+  · apply Perm3.swap23
+  · apply Perm3.swap23
+  done
+
+theorem ev_inversion : ∀ (n : Nat),
+  ev n →
+  (n = 0) ∨ (∃ n', n = n' + 2 ∧ ev n') := by
+  intro n h
+  cases h
+  case zero =>
+    left
+    rfl
+  case succ n' h' =>
+    right
+    -- exists n'  -- 別解
+    apply Exists.intro n'
+    constructor
+    case left =>
+      rfl
+    case right =>
+      apply h'
+  done
+
+theorem le_inversion
+  : ∀ (n m : Nat),
+  n ≤ m →
+  (n = m) ∨ (∃ m', m = m' + 1 ∧ n ≤ m') := by
+  intro n m h
+  cases h
+  case refl =>
+    left
+    rfl
+  case step m h' =>
+    right
+    apply Exists.intro m
+    constructor
+    case left =>
+      rfl
+    case right =>
+      apply h'
+  done
+
+theorem evSS_ev : ∀ n, ev (n + 2) → ev n := by
+  intro n h
+  have h' := ev_inversion _ h
+  rcases h' with (h0 | h1)
+  · nomatch h0
+  · rcases h1 with ⟨n', h2, h3⟩
+    simp at h2
+    rw [h2]
+    apply h3
+  done
+
+theorem evSS_ev' : ∀ n, ev (n + 2) → ev n := by
+  intro n h
+  cases h
+  case succ h' =>
+    apply h'
+    done
+
+theorem one_not_even : ¬ ev 1 := by
+  intro h
+  have h' := ev_inversion _ h
+  rcases h' with (h0 | ⟨m, hm, _⟩)
+  · nomatch h0
+  · nomatch hm
+
+theorem one_not_even' : ¬ ev 1 := by
+  intro h
+  nomatch h
+
+theorem SSSSev__even : ∀ n,
+  ev (n + 4) → ev n := by
+  intro n h
+  rcases h with (_ | (_ | h'))
+  exact h'
+
+theorem ev5_nonsense :
+  ev 5 → 2 + 2 = 9 := by
+  intro h
+  rcases h with (_ | (_ | h'))
+  nomatch h'
+
+theorem inversion_ex1 : ∀ (n m o : Nat),
+  [n,  m] = [o,  o] → [n] = [m] := by
+  intro n m o h
+  cases h
+  rfl
+
+theorem inversion_ex2 : ∀ (n : Nat),
+  n + 1 = 0 → 2 + 2 = 5 := by
+  intro n contra
+  cases contra
+  done
+
+theorem ev_Even_firsttry : ∀ n, ev n → Even n := by
+  unfold Even
+  intro n h
+  rcases h with (heq | ⟨n', h1, heq⟩)
+  · exists 0
+  · exists 1
+  · sorry
